e028c70e858bfb47ad54a8c6eb32ab83e6db05d0
Donghyun Lee <jojo7522@gmail.com>
2016-12-15 21:05:16 +0900

    venezia final ver.
    Signed-off-by: Donghyun Lee <jojo7522@gmail.com>

--- venezia.c
+++ venezia.c
@@ -12,11 +12,12 @@ typedef struct drop_word{
 }drop_word;
 
 int score=0;
+int scoreText[3];
 int life = 10;
 char lifeText[2];
 int j=0, i=0;
 int sleep_time = 1;
-int row = 0, col = 0;
+
 char word[50][10] = {"apple","bus","truck","exam","best",
         "awful","awesome", "computer", "linux", "data", 
 	"bat", "adult", "birth", "resource", "score",
@@ -29,16 +30,27 @@ char word[50][10] = {"apple","bus","truck","exam","best",
 	"ahead", "handle", "include", "press", "push"};
 drop_word *drop;
 
+void init_venezia()
+{
+	score = 0;
+	life = 10;
+	strcpy(scoreText, "0");
+	strcpy(lifeText, "0");
+	j = 0;
+	i = 0;
+}
 void makeDrop()
 {
-	if(i>=50)
-		return;
-	col = rand()%(COLS-1);
+	int col = 0;
+	if(i<50)
+	{	
+	col = rand()%(COLS-10);
 	move(0, col);
 	drop[i].row = 0;
 	drop[i].col = col;
 	drop[i].exist = true;
 	strcpy(drop[i++].words, word[rand()%50]);
+	}
 }
 
 
@@ -88,11 +100,16 @@ void find(char *str)
 {
 	for(j=0; j<i; j++)
 	{
-		if(!strcmp(drop[j].words, str))
+		if(drop[j].exist == true && !strcmp(drop[j].words, str))
 		{
 			drop[j].exist = false;
 			strcpy(drop[j].words, "");
 			score++;
+			move(LINES-1, 6);
+			addstr("  ");
+			move(LINES-1, 6);
+			sprintf(scoreText, "%d", score*100);
+			addstr(scoreText);
 			return;
 		}
 	}
@@ -104,9 +121,10 @@ void venezia_game(){
 	char input[20]= {0};
 	int o;
 	int k;
-	char scoreText[2];
 	srand( (unsigned)time(NULL) );
 	drop = (drop_word*)malloc(sizeof(drop_word)*50);
+	
+	init_venezia();
 
 	clear();
 	move(LINES-2, 0);
@@ -114,14 +132,18 @@ void venezia_game(){
 	{
 		addstr("-");
 	}
-	move(LINES-1,0);
+	move(LINES-1, 0);
+	addstr("score:");
+	sprintf(scoreText, "%d", score*100);
+	addstr(scoreText);
+	move(LINES-1,(COLS-1)/2-7);
 	addstr("Enter:");
-	for(k=0; k<COLS-15;k++)
-		addstr(" ");
-	addstr("life=");
+	move(LINES-1, COLS-9);
+	addstr("life:");
 	sprintf(lifeText, "%d", life);
 	addstr(lifeText);
-	refresh();	
+
+	refresh();
 	pthread_create(&t1, NULL, print, NULL);
 	while(life>0 && (score+10-life) != 50){
 		for(length = 0; length < 20;)
@@ -140,11 +162,14 @@ void venezia_game(){
 				{
 					input[k] = '\0';
 				}
-				move(LINES-1,0);
+				move(LINES-1, 0);
+				addstr("score:");
+				move(LINES-1,(COLS-1)/2-7);
 				addstr("Enter:");
-				for(k=0; k<COLS-15;k++)
+				for(k=0; k<=length;k++)
 					addstr(" ");
-				addstr("life=");
+				move(LINES-1, COLS-9);
+				addstr("life:");
 				move(LINES-1, (COLS-1)/2);
 				break;
 			}
@@ -178,22 +203,21 @@ void venezia_game(){
 		if( (score+10-life) == 50)
 			break;
 	}
-	pthread_join(&t1, NULL);
 	free(drop);
 	clear();
 	move((LINES-1)/2, (COLS-1)/2);
 	if(life>0)
 	{
-		addstr("Clear");
+		addstr("Congratulation!");
 		move((LINES-1)/2+1, (COLS-1)/2);
 		addstr("Score: ");
-		sprintf(scoreText, "%d", score);
+		sprintf(scoreText, "%d", score*100);
 		addstr(scoreText);
 	}
 	else
 		addstr("Game Over");
-//	move(LINES-1, (COLS-1)/2);
-//	addstr("Press Any Key");
+	move(LINES-1, (COLS-1)/2);
+	addstr("Press Any Key");
 	refresh();
 	getch();
 }
