4bac47dd72c626c1041265b65db3683805ffa7c6
Donghyun Lee <jojo7522@gmail.com>
2016-12-15 18:47:49 +0900

    almost done
    Signed-off-by: Donghyun Lee <jojo7522@gmail.com>

--- venezia.c
+++ venezia.c
@@ -17,7 +17,15 @@ int j=0, i=0;
 int sleep_time = 1;
 int row = 0, col = 0;
 char word[50][10] = {"apple","bus","truck","exam","best",
-        "awful","awesome", "computer", "linux", "data"};
+        "awful","awesome", "computer", "linux", "data", 
+	"bat", "adult", "birth", "resource", "score",
+	"type", "vegetable", "tea", "tax", "tongue",
+	"absent", "careful", "flat", "main", "print",
+	"length", "integer", "double", "string", "solar",
+	"straight", "silly", "rapid", "cure", "deliver",
+	"draw", "force", "gain", "remain", "review", 
+	"scream", "pray", "tear", "vote", "smell",
+	"ahead", "handle", "include", "press", "push"};
 drop_word *drop;
 
 void makeDrop()
@@ -29,7 +37,7 @@ void makeDrop()
 	drop[i].row = 0;
 	drop[i].col = col;
 	drop[i].exist = true;
-	strcpy(drop[i++].words, word[rand()%10]);
+	strcpy(drop[i++].words, word[rand()%50]);
 }
 
 
@@ -43,17 +51,34 @@ void thread_1(void *none)
 		makeDrop();
 		for(j=0;j<i;j++)
 		{
+			if(drop[j].row>LINES-3)
+				continue;
 			move(drop[j].row,0);
 			for(k=0; k<COLS; k++)
 				addstr(" ");
+			if(j==49)
+			{
+				move(drop[j].row-1, 0);
+				for(k=0; k<COLS; k++)
+					addstr(" ");
+				
+			}				
 			if(drop[j].exist == true)
 			{
 				move(drop[j].row,drop[j].col);
 				addstr(drop[j].words);
-				if(drop[j].row >= COLS-3)
+				if(drop[j].row > LINES-3)
 				{
 					drop[j].exist = false;
+					strcpy(drop[j].words, "");
 					life--;
+					move(LINES-1, COLS-4);
+					addstr("   ");
+					sprintf(lifeText, "%d", life);
+					move(LINES-1, COLS-4);
+					addstr(lifeText);
+					if(life<0)
+						break;
 				}
 			}
 			drop[j].row++;
@@ -80,12 +105,12 @@ void venezia_game(){
 	pthread_t t1;
 	int length = 20;
 	char input[20]= {0};
-	int c;
+	int o;
 	int k;
 	srand( (unsigned)time(NULL) );
 	drop = (drop_word*)malloc(sizeof(drop_word)*50);
 	
-	
+	clear();
 	clear();
 	move(LINES-2, 0);
 	for(k=0; k<COLS-1; k++)
@@ -104,8 +129,8 @@ void venezia_game(){
 	while(life>0){
 		for(length = 0; length < 20;)
 		{
-			c = getch();
-			if(c == '\n')
+			o = getch();
+			if(o == '\n')
 			{
 				input[length] = '\0';
 				find(input);
@@ -122,30 +147,34 @@ void venezia_game(){
 				sprintf(lifeText, "%d", life);
 				addstr(lifeText);
 				move(LINES-1, (COLS-1)/2);
+				break;
 			}
-			else if ( c== 127)
+			else if ( o == 127)
 			{
 				if( length > 0)
 				{
 					input[--length] = '\0';
 					move(LINES-1, (COLS-1)/2);
-					addstr("                    ");
+					addstr("               ");
 					move(LINES-1, (COLS-1)/2);
 					addstr(input);
 				}
 				else
 				{
 					move(LINES-1, (COLS-1)/2);
-					addstr("                    ");
+					addstr("               ");
+					move(LINES-1, (COLS-1)/2);
 				}
 			}
 			else
 			{
-				input[length++] = c;
+				input[length++] = o;
 				move(LINES-1, (COLS-1)/2);
 				addstr(input);
 			}
 			refresh();
+			if(life<0)
+				break;
 		}	
 	}
 	pthread_join(&t1, NULL);
